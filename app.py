9185c51790f6a3c681111a791724e2ff0f1466d1
Haitham Ibrahim <haysemibro@gmail.com>
2023-11-26 16:41:31 -0600

    Refactor app.py to use transcribe_audio

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, jsonify
+from transcribe import transcribe_audio
+from threading import Thread
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/transcribe', methods=['POST'])
+def transcribe():
+    if 'audio' not in request.files:
+        return jsonify({'error': 'No audio file provided'})
+
+    audio_file = request.files['audio']
+
+    if audio_file.filename == '':
+        return jsonify({'error': 'No selected file'})
+
+    try:
+        transcription_thread = Thread(
+            target=transcribe_audio, args=(audio_file,)
+        )
+        transcription_thread.start()
+        return jsonify({'message': 'Transcription started'})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
