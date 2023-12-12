f5fd2e0c9b445afd6497df59ba54ee7c67cc8851
Haitham Ibrahim <haysemibro@gmail.com>
2023-12-11 22:19:16 -0600

    Project ready for deployment.

--- app.py
+++ app.py
@@ -32,4 +32,5 @@ def translate():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
